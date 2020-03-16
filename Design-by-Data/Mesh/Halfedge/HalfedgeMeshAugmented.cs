a1db367c96fec6d41bc1a60bfa17f6431f33b0e3
Romain Mesnil <romain.mesnil@enpc.fr>
2020-03-16 14:09:51 +0100

    Implementation of Half-edge meshes

--- Design-by-Data/Mesh/Halfedge/HalfedgeMeshAugmented.cs
+++ Design-by-Data/Mesh/Halfedge/HalfedgeMeshAugmented.cs
@@ -8,11 +8,21 @@ using PlanktonGh;
 
 namespace Design_by_Data.Mesh.Halfedge
 {
+    /// <summary>
+    /// A class describing a halfedge mesh with additional properties
+    /// </summary>
     class HalfedgeMeshAugmented
     {
+        /// <summary>
+        /// A PlanktonMesh (half-edge mesh)
+        /// </summary>
         public Plankton.PlanktonMesh PMesh { get; set; }
-
+        
         #region Constructors 
+        /// <summary>
+        /// Constructs a Halfedge augmented mesh from
+        /// </summary>
+        /// <param name="mesh"></param>
         public HalfedgeMeshAugmented(Rhino.Geometry.Mesh mesh)
         {
             PMesh = new Plankton.PlanktonMesh();
