a775344121f6c7745ffe079e216c7514c81012e1
Romain Mesnil <romain.mesnil@enpc.fr>
2020-04-05 23:10:34 +0200

    second commit: an example of laplacian smoothing

---
+++ Design-by-Data/Grasshopper Component/LaplacianSmoothing.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Design_by_Data.Mesh.Halfedge;
+
+namespace Design_by_Data.Grasshopper_Component
+{
+    public class LaplacianSmoothing : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the LaplacianSmoothing class.
+        /// </summary>
+        public LaplacianSmoothing()
+          : base("LaplacianSmoothing", "LapSmooth",
+              "Laplcian smoothing of a mesh",
+              "Design by Data", "MANGT")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddMeshParameter("mesh", "mesh", "a mesh", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("nStep", "nStep", "nStep", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Lambda", "L", "Damping value", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddMeshParameter("mesh", "mesh", "a mesh", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Rhino.Geometry.Mesh inputmesh = new Rhino.Geometry.Mesh();
+            double lambda = 0.0;
+            int n = 0;
+            DA.GetData(0, ref inputmesh);
+            DA.GetData(1, ref n);
+            DA.GetData(2, ref lambda);
+            HalfedgeMeshAugmented mesh = new HalfedgeMeshAugmented(inputmesh);
+            mesh.LaplacianSmoothing(n, lambda);
+            DA.SetData(0, mesh.ToRhinoMesh());
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("c1ef1998-ab5c-47c9-8b72-aa45806e2292"); }
+        }
+    }
+}
--- Design-by-Data/Mesh/Halfedge/HalfedgeMeshAugmented.cs
+++ Design-by-Data/Mesh/Halfedge/HalfedgeMeshAugmented.cs
@@ -28,6 +28,62 @@ namespace Design_by_Data.Mesh.Halfedge
             PMesh = new Plankton.PlanktonMesh();
             PMesh = mesh.ToPlanktonMesh();
         }
+
+        #endregion
+        #region Methods
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nStep"></param>
+        /// <param name="lambda">Damping factor, comprised between 0 and 1, 1 is the full laplacian smoothing step</param>
+        public void LaplacianSmoothing(int nStep, double lambda)
+        {
+            
+            for (int k = 0; k < nStep; k++)
+            {
+                List<double> x = new List<double>();
+                List<double> y = new List<double>();
+                List<double> z = new List<double>();
+                for (int i = 0; i < PMesh.Vertices.Count; i++)
+                {
+                    var neighbours = PMesh.Vertices.GetVertexNeighbours(i);
+                    
+                    int n = neighbours.Length;
+                    var dx = 0.0;
+                    var dy = 0.0;
+                    var dz = 0.0;
+                    if(PMesh.Vertices.IsBoundary(i))
+                    {
+                        x.Add(PMesh.Vertices[i].X);
+                        y.Add(PMesh.Vertices[i].Y);
+                        z.Add(PMesh.Vertices[i].Z);
+                    }
+                    else
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            dx += PMesh.Vertices[neighbours[j]].X / n;
+                            dy += PMesh.Vertices[neighbours[j]].Y / n;
+                            dz += PMesh.Vertices[neighbours[j]].Z / n;
+                        }
+                        x.Add(lambda * dx + (1 - lambda) * PMesh.Vertices[i].X);
+                        y.Add(lambda * dy + (1 - lambda) * PMesh.Vertices[i].Y);
+                        z.Add(lambda * dz + (1 - lambda) * PMesh.Vertices[i].Z);
+                    }
+                }
+                for (int i = 0; i < PMesh.Vertices.Count; i++)
+                {
+                    PMesh.Vertices[i].X = (float)x[i];
+                    PMesh.Vertices[i].Y = (float)y[i];
+                    PMesh.Vertices[i].Z = (float)z[i];
+                }
+            }
+        }
+        public Rhino.Geometry.Mesh ToRhinoMesh()
+        {
+            return (PMesh.ToRhinoMesh());
+        }
+        
         #endregion
     }
 }
